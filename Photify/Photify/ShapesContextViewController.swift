d1a0c8906d13ae398b1d4bd2448cb2b97c61d008
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-08 18:25:32 +0300

    Add bottom container for controls, ability to open app in a spit screen

--- Photify/Photify/ShapesContextViewController.swift
+++ Photify/Photify/ShapesContextViewController.swift
@@ -8,36 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ShapesContextViewController: UIViewController {
+    @IBOutlet weak var bottomControlsContainerView: UIView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         let rectangle = ShapeView(shape: Rectangle(), frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        self.view.addSubview(rectangle)
+        self.scrollView.addSubview(rectangle)
         
         let hexagon = ShapeView(shape: Hexagon())
         hexagon.frame = CGRect(x: 100, y: 300, width: 200, height: 100)
         hexagon.layer.borderWidth = 1.0
-        self.view.addSubview(hexagon)
+        self.scrollView.addSubview(hexagon)
         
         let circle = ShapeView(shape: Circle())
         circle.frame = CGRect(x: 100, y: 300, width: 200, height: 100)
         circle.fillColor = nil
         circle.lineWidth = 1.0
         circle.shapeLayer.strokeColor = UIColor.blue.cgColor
-        self.view.addSubview(circle)
+        self.scrollView.addSubview(circle)
         
         let ellipse = ShapeView(shape: Ellipse())
         ellipse.frame = CGRect(x: 100, y: 600, width: 300, height: 100)
-        self.view.addSubview(ellipse)
+        self.scrollView.addSubview(ellipse)
         
         let triangle = ShapeView(shape: Triangle(), frame: CGRect(x: 300, y: 100, width: 100, height: 100))
-        self.view.addSubview(triangle)
+        self.scrollView.addSubview(triangle)
     }
 }
 
