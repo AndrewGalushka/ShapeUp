85aacb03f5d5e496968a744fb9f340071dfc5fa7
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-15 18:31:57 +0300

    Add CanvasEntity, CanvasListViewController, CanvasListView

---
+++ Photify/Photify/Screens/CanvasListView/CanvasListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasListViewController.swift
+//  Photify
+//
+//  Created by Galushka on 7/15/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class CanvasListViewController: UIViewController {    
+}
---
+++ Photify/Photify/Screens/CanvasListView/CanvasesTableView/CanvasListView.swift
@@ -0,0 +1,68 @@
+//
+//  CanvasListTableView.swift
+//  Photify
+//
+//  Created by Galushka on 7/15/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CanvasListView: UIView {
+    @IBInspectable private lazy var tableView: UITableView = {
+        let tb = UITableView(frame: .zero)
+        
+        self.addSubview(tb)
+        
+        if self.translatesAutoresizingMaskIntoConstraints {
+            tb.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        } else {
+            tb.translatesAutoresizingMaskIntoConstraints = false
+            tb.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            tb.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            tb.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+            tb.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        }
+        
+        return tb
+    }()
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+           return self.layer.cornerRadius
+        }
+        
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    private func setUp() {
+        tableView.backgroundColor = .red
+        tableView.dataSource = self
+    }
+}
+
+extension CanvasListView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+}
