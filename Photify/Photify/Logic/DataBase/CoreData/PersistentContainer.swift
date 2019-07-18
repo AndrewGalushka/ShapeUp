47b9af862c6381d2788e9796ecc8109967e22c74
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-18 12:26:39 +0300

    rf

--- Photify/Photify/Logic/DataBase/CoreData/PersistentContainer.swift
+++ Photify/Photify/Logic/DataBase/CoreData/PersistentContainer.swift
@@ -16,13 +16,11 @@ final class PersistentContainer: NSPersistentContainer {
         // performAndWait or perform - is thread save approach as mensioned on
         // https://developer.apple.com/documentation/coredata/setting_up_a_core_data_stack/setting_up_a_core_data_stack_manually
         
-//        context.performAndWait {
-            guard context.hasChanges else { return }
-            do {
-                try context.save()
-            } catch let error as NSError {
-                print("Error: \(error), \(error.userInfo)")
-            }
-//        }
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
     }
 }
