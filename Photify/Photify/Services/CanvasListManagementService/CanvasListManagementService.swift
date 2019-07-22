f1efd10486f44f3ac3f717067b6c556c32c58db8
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-22 11:58:57 +0300

    Add CanvasListManagementService

---
+++ Photify/Photify/Services/CanvasListManagementService/CanvasListManagementService.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasListManagementService.swift
+//  Photify
+//
+//  Created by Galushka on 7/22/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+class CanvasListManagementService: CanvasListManagementServiceProtocol {
+}
---
+++ Photify/Photify/Services/CanvasListManagementService/CanvasListManagementServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasListManagementServiceProtocol.swift
+//  Photify
+//
+//  Created by Galushka on 7/22/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+protocol CanvasListManagementServiceProtocol: AnyObject {
+}
