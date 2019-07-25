5ded3edf4fd1d3b60fbe138304ddd5821e6802dc
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-25 17:36:36 +0300

    Correct expand element size and initial state, omit other directions and focus only on .up type

--- Photify/Photify/Controls/ColorPicker/ColorPicker.swift
+++ Photify/Photify/Controls/ColorPicker/ColorPicker.swift
@@ -12,19 +12,11 @@ import UIKit
 class ColorPicker: UIView {
     
     @IBInspectable
-    private(set) var isExpanded: Bool = false {
-        didSet {
-            if isExpanded {
-                expandingContainer.backgroundColor = .cyan
-            } else {
-                expandingContainer.backgroundColor = .red
-            }
-        }
-    }
+    private(set) var isExpanded: Bool = false
     
     private(set) var expandDirection: ExpandDirection = .up {
         didSet {
-            setup()
+            assert(ExpandDirection.up == expandDirection, "Only .up direction supports, other types will be implemented later")
         }
     }
     
@@ -35,6 +27,8 @@ class ColorPicker: UIView {
         view.backgroundColor = .blue
         return view
     }()
+    
+    var expandingContainerHeightConstraint: NSLayoutConstraint?
     
     // MARK: - Lifecycle
     
@@ -50,21 +44,45 @@ class ColorPicker: UIView {
         self.setup()
     }
     
+    var counter = 0
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        self.constraints.forEach {
+            print($0)
+        }
+        
+        print("updateConstraints: \(self.bounds.height)")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.updateLayoutForExpandingContainer()
+        print("layoutSubviews: \(self.bounds.height)")
+    }
+    
+    override func layoutMarginsDidChange() {
+        super.layoutMarginsDidChange()
+        print("layoutMarginsDidChange: \(self.bounds.height)")
+    }
+    
     // MARK: - Methods(Public)
     
     func toggleIsExpanded() {
+        self.expandDirection = .down
         
         let startingPath: CGPath
         let finalPath: CGPath
         
         if self.isExpanded {
-            startingPath = CGPath(rect: self.expandingContainer.bounds, transform: nil)
-            finalPath = CGPath(rect: CGRect(x: 0, y: expandingContainer.bounds.height, width: expandingContainer.bounds.width, height: 0),
-                                   transform: nil)
-        } else {
             startingPath = CGPath(rect: CGRect(x: 0, y: expandingContainer.bounds.height, width: expandingContainer.bounds.width, height: 0),
                                   transform: nil)
             finalPath = CGPath(rect: self.expandingContainer.bounds, transform: nil)
+        } else {
+            startingPath = CGPath(rect: self.expandingContainer.bounds, transform: nil)
+            finalPath = CGPath(rect: CGRect(x: 0, y: expandingContainer.bounds.height, width: expandingContainer.bounds.width, height: 0),
+                               transform: nil)
         }
         
         let maskLayer = CAShapeLayer()
@@ -76,6 +94,8 @@ class ColorPicker: UIView {
         animation.toValue = finalPath
         animation.duration = 0.25
         animation.timingFunction = .init(name: CAMediaTimingFunctionName.easeOut)
+        
+        maskLayer.add(animation, forKey: nil)
         
         CATransaction.begin()
         CATransaction.setDisableActions(true)
@@ -90,6 +110,7 @@ class ColorPicker: UIView {
     
     private func setup() {
         self.configureInitialUI()
+        expandingContainer.layer.mask = CAShapeLayer()
     }
     
     private func configureInitialUI() {
@@ -109,9 +130,6 @@ class ColorPicker: UIView {
     }
     
     private func embedExpandingContainer() {
-        self.setNeedsLayout()
-        self.layoutIfNeeded()
-        
         self.expandingContainer.removeFromSuperview()
         self.expandingContainer.translatesAutoresizingMaskIntoConstraints = false
         self.insertSubview(expandingContainer, belowSubview: parentCircle)
@@ -119,24 +137,41 @@ class ColorPicker: UIView {
         let containerSize = calculateContainerSize()
         
         self.expandingContainer.widthAnchor.constraint(equalToConstant: containerSize.width).isActive = true
-        self.expandingContainer.heightAnchor.constraint(equalToConstant: containerSize.height).isActive = true
+        self.expandingContainerHeightConstraint = self.expandingContainer.heightAnchor.constraint(equalToConstant: containerSize.height)
+        expandingContainerHeightConstraint?.isActive = true
         
         switch expandDirection {
         case .up:
             expandingContainer.centerXAnchor.constraint(equalTo: parentCircle.centerXAnchor).isActive = true
             expandingContainer.bottomAnchor.constraint(equalTo: parentCircle.topAnchor, constant: -configs.insetsBetweenCircles).isActive = true
         case .down:
-            expandingContainer.centerXAnchor.constraint(equalTo: parentCircle.centerXAnchor).isActive = true
-            expandingContainer.topAnchor.constraint(equalTo: parentCircle.bottomAnchor, constant: -configs.insetsBetweenCircles).isActive = true
+            fatalError("Unsapported Type, need to be implemented later")
+//            expandingContainer.centerXAnchor.constraint(equalTo: parentCircle.centerXAnchor).isActive = true
+//            expandingContainer.topAnchor.constraint(equalTo: parentCircle.bottomAnchor, constant: -configs.insetsBetweenCircles).isActive = true
         case .left:
-            expandingContainer.centerYAnchor.constraint(equalTo: parentCircle.centerYAnchor).isActive = true
-            expandingContainer.leftAnchor.constraint(equalTo: parentCircle.trailingAnchor, constant: -configs.insetsBetweenCircles)
+            fatalError("Unsapported Type, need to be implemented later")
+//            expandingContainer.centerYAnchor.constraint(equalTo: parentCircle.centerYAnchor).isActive = true
+//            expandingContainer.leftAnchor.constraint(equalTo: parentCircle.trailingAnchor, constant: -configs.insetsBetweenCircles)
         case .right:
-            expandingContainer.centerYAnchor.constraint(equalTo: parentCircle.centerYAnchor).isActive = true
-            expandingContainer.rightAnchor.constraint(equalTo: parentCircle.leftAnchor, constant: -configs.insetsBetweenCircles)
+            fatalError("Unsapported Type, need to be implemented later")
+//            expandingContainer.centerYAnchor.constraint(equalTo: parentCircle.centerYAnchor).isActive = true
+//            expandingContainer.rightAnchor.constraint(equalTo: parentCircle.leftAnchor, constant: -configs.insetsBetweenCircles)
         }
     }
 
+    func updateLayoutForParentCircle() {
+        
+    }
+    
+    func updateLayoutForExpandingContainer() {
+        guard let expandingContainerHeightConstraint = self.expandingContainerHeightConstraint else { return }
+        let containerSize = calculateContainerSize()
+        
+        assert(self.expandDirection == ExpandDirection.up, fatalError("Only .up direction supports, other types will be implemented later"))
+        
+        expandingContainerHeightConstraint.constant = containerSize.height
+    }
+    
     private func calculateContainerSize() -> CGSize {
         let minItemLength = self.configs.circleDiameter
         let availableHeight = self.bounds.height - configs.circleDiameter - configs.insetsBetweenCircles
@@ -165,7 +200,7 @@ extension ColorPicker {
         let insetsBetweenCircles: CGFloat = 8.0
     }
     
-    @objc enum ExpandDirection: Int {
+    enum ExpandDirection: Int {
         case up
         case down
         case left
