fc4380449521614863186dfee86777c39814c59a
Andrew Galushka <andrewgalushka@yahoo.com>
2019-07-26 16:53:22 +0300

    Add Color Model

---
+++ Photify/Photify/Models/Color/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Photify
+//
+//  Created by Galushka on 7/26/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+
+struct Color: RGBAColor {
+    let red, green, blue, alpha: Int8
+}
---
+++ Photify/Photify/Models/Color/RGBAColor.swift
@@ -0,0 +1,16 @@
+//
+//  RGBAColor.swift
+//  Photify
+//
+//  Created by Galushka on 7/26/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+protocol RGBAColor {
+    var red: Int8 { get }
+    var green: Int8 { get }
+    var blue: Int8 { get }
+    var alpha: Int8 { get }
+}
