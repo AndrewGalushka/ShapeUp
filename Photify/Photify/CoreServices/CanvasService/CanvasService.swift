5aee4efbe97e7389615213d08d76fd96a7818d97
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-01 16:17:53 +0300

    Add CanvasService class

---
+++ Photify/Photify/CoreServices/CanvasService/CanvasService.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasService.swift
+//  Photify
+//
+//  Created by Galushka on 8/1/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+class CanvasService: CanvasServiceProtocol {
+}
---
+++ Photify/Photify/CoreServices/CanvasService/CanvasServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasServiceProtocol.swift
+//  Photify
+//
+//  Created by Galushka on 8/1/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+protocol CanvasServiceProtocol {
+}
