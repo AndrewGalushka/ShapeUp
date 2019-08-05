4ab91d3172e723c29e5014c77a277efef9229183
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-05 18:15:44 +0300

    [CoreDataEntityMapper+Canvases] Extend mapping for shapes support

--- Photify/DataStorage/Mappers/Canvas/CoreDataEntityMapper+Canvas.swift
+++ Photify/DataStorage/Mappers/Canvas/CoreDataEntityMapper+Canvas.swift
@@ -26,14 +26,26 @@ extension CoreDataEntityMapper: CanvasMapable {
         return canvasEntities
     }
     
-    private func mapOut(_ Canvas: Canvas) -> CanvasDTO {
-        return CanvasDTO(identifier: Canvas.identifier!, name: Canvas.name ?? "")
+    private func mapOut(_ canvas: Canvas) -> CanvasDTO {
+        var canvasDTO = CanvasDTO(identifier: canvas.identifier!, name: canvas.name ?? "")
+        
+        if let shapes = (canvas.shapes?.compactMap { $0 as? ShapeView }) {
+            canvasDTO.shapes = self.mapOut(shapeViews: shapes)
+        }
+        
+        return canvasDTO
     }
     
     private func mapIn(_ canvasDTO: CanvasDTO, moc: NSManagedObjectContext) -> Canvas {
         let canvas = Canvas(context: moc)
         canvas.identifier = canvasDTO.identifier
         canvas.name = canvasDTO.name
+        
+        let shapesDTOs = self.mapIn(shapeViewsDTOs: canvasDTO.shapes, moc: moc)
+        
+        if !shapesDTOs.isEmpty  {
+            canvas.addToShapes(NSSet(array: shapesDTOs))
+        }
         
         return canvas
     }
