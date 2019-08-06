36221573216e64f70be10d07484f47cdd2ea7e27
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-06 19:06:46 +0300

    Add ColorDTO

---
+++ Photify/DataStorage/EntitiyTypes/Color/ColorDTO.swift
@@ -0,0 +1,16 @@
+//
+//  ColorDTO.swift
+//  DataStorage
+//
+//  Created by Galushka on 8/6/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+struct ColorDTO {
+    var red: Float
+    var green: Float
+    var blue: Float
+    var alpha: Float
+}
