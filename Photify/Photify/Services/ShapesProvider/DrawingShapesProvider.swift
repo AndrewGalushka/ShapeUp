a599315406231f053dda5daf8c2ba55f8a94d7e2
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-12 17:51:02 +0300

    Refactor and add new functionality to ShapesProvider

---
+++ Photify/Photify/Services/ShapesProvider/DrawingShapesProvider.swift
@@ -0,0 +1,49 @@
+//
+//  DrawingShapesProvider.swift
+//  Photify
+//
+//  Created by Galushka on 7/24/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+class DrawingShapesProvider {
+    
+    func allExistingShapeTypes() -> [ShapeType] {
+        return ShapeType.AllCases()
+    }
+    
+    func allExistingShapes() -> [Shape] {
+        return [XMark(),
+                Circle(),
+                Ellipse(),
+                Hexagon(),
+                Triangle(),
+                Star(),
+                Rectangle()]
+    }
+    
+    func makeShape(from shapeType: ShapeType) -> Shape {
+        return DrawingShapesProvider.makeShape(from: shapeType)
+    }
+    
+    static func makeShape(from shapeType: ShapeType) -> Shape {
+        switch shapeType {
+        case .rectangle:
+            return Rectangle()
+        case .circle:
+            return Circle()
+        case .ellipse:
+            return Ellipse()
+        case .hexagon:
+            return Hexagon()
+        case .triangle:
+            return Triangle(alignment: .center)
+        case .star:
+            return Star()
+        case .xMark:
+            return XMark()
+        }
+    }
+}
