0835bb6cc159c3585d3f1f073a611c5de4708271
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-13 12:14:34 +0300

    Add ShapesDragAndDropDTO

---
+++ Photify/Photify/Models/ShapesDragAndDropDTO/ShapesDragAndDropDTO.swift
@@ -0,0 +1,14 @@
+//
+//  ShapesDragAndDropDTO.swift
+//  Photify
+//
+//  Created by Galushka on 8/13/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+struct ShapesDragAndDropDTO {
+    var shapeType: ShapeType
+    var shapeStyle: ShapeStyle
+}
