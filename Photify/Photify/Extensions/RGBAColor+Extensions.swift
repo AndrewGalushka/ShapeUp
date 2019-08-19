aee9a3a8a99abc67d85eb07bf6a2410c4f8f5a5b
Andrew Galushka <andrewgalushka@yahoo.com>
2019-08-19 18:33:41 +0300

    Rename UIColor+Extensions to RGBAColor+Extensions

---
+++ Photify/Photify/Extensions/RGBAColor+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  RGBAColor+Extensions.swift
+//  Photify
+//
+//  Created by Galushka on 7/26/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(rgbaColor: RGBAColor) {
+        self.init(red: CGFloat(rgbaColor.red),
+                  green: CGFloat(rgbaColor.green),
+                  blue: CGFloat(rgbaColor.blue),
+                  alpha: CGFloat(rgbaColor.alpha))
+    }
+}
+
+extension RGBAColor {
+    init(uiColor: UIColor) {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        self.init(red: Float(red),
+                  green: Float(green),
+                  blue: Float(blue),
+                  alpha: Float(alpha))
+    }
+    
+    var uiColor: UIColor {
+        return .init(rgbaColor: self)
+    }
+}
