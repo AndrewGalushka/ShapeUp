455ad3f13b3cef644046b9a12e111e199b8e0b88
Andrew Galushka <andrewgalushka@yahoo.com>
2019-09-12 18:00:37 +0300

    [ShapesProvider] Change method name

--- Photify/Photify/Services/ShapesProvider/ShapesProvider.swift
+++ Photify/Photify/Services/ShapesProvider/ShapesProvider.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct ShapesProvider: ShapeTypeToShapeConvertible {
 
-    func allExistingShapeTypes() -> [ShapeType] {
+    func allShapeTypes() -> [ShapeType] {
         return ShapeType.allCases
     }
     
